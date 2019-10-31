6c0833fccba10582698162ed4c8d55b3061e28b1
Zach <abc@hotmail.co>
2019-10-31 17:00:31 +0000

    Add dive count to index screen

--- HTML/index.js
+++ HTML/index.js
@@ -3,17 +3,20 @@ function studentsToTable(){
   const url = 'http://35.246.94.238:9002/showAll';
   Http.open("GET", url);
   Http.onreadystatechange = function(e){
-        if (Http.readyState==4){
+  if (Http.readyState==4){
     data=JSON.parse(Http.responseText);
+    var startTime;
+    var finishTime;
+    var totalTime = 0;
     var A=0;
     data.forEach(function(item){
       var table = document.getElementById("Results");
       var row = table.insertRow(-1);
-      var cell1 = row.insertCell(0);
-      var cell2 = row.insertCell(1);
-      var cell3 = row.insertCell(2);
-      var cell4 = row.insertCell(3);
-      cell1.innerHTML = item.diveid;
+      //var cell1 = row.insertCell(0);
+      var cell2 = row.insertCell(0);
+      var cell3 = row.insertCell(1);
+      var cell4 = row.insertCell(2);
+      //cell1.innerHTML = item.diveid;
       cell2.innerHTML = item.location;
       cell3.innerHTML = item.divedate;
       cell4.innerHTML = item.weight;
@@ -22,8 +25,10 @@ function studentsToTable(){
         location.replace("EditPage.html#"+item.diveid);
       });
       A=A+1;
+      document.getElementById("diveCountNumber").innerHTML = A;
     });
   }
 } 
-  Http.send();
+  Http.send()
+
 }
